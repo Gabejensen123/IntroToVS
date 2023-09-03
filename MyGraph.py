a0da36417100ce7d234260fee7a4415ea6a6036f
Gabe Jensen <Gabe_jensen1@baylor.edu>
2023-09-03 09:30:53 -0500

    commit

--- MyGraph.py
+++ MyGraph.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt #allows you to create graphs in python from a thirdparty database. 
 import numpy as np #numpy is apart of the matplotlib.pyplot library 
+#1. importing matplotlib.pyplot as plt - we are getting our tool ready to work
+#2. we are getting a helper called numpy ready to help with the numbers. 
 
-x = np.linspace(0, 20,100)
-plt.plot(x, np.sin(x))
-plt.show()
+x = np.linspace(0, 20,100) #tells our computer to make a line of 100 points starting from 0 and going up to 20. 
+plt.plot(x, np.sin(x))# this draws a sign curve that connects all the dots
+plt.show()#simply showing the picture that we created in the function
 
-#pythong is an open source language, you can use anyones code in the world. Upsidem you dont pay for anything. 
+
+#python is an open source language, you can use anyones code in the world. Upsidem you dont pay for anything. 
 #it has a an extenseive set of libraries
 # there is no governance, when you intall these libraries they can have overlap and errors. They interfere with memory and CPU space. 
 # best practices, install a Virtual environment - creates a folder and makes a copy of your python interpreter. 
 # quiz 1 how to create a virtual environment, install a virtual environment. 
 
-#Step 1 - creating a virtual environment - folder that makes a copy of your python files. #naming convention py -3 -m venv introvenv#names of the new environment
+#Step 1 - creating a virtual environment - folder that makes a copy of your python files. #naming convention py -3 -m venv this is the new naming convention,  introvenv#names of the new environment
 #That way this has access to the python interpreter and subsequent libraries.It made a copy of the python.exe, the things that runs pything
 # 
 # 
@@ -25,3 +28,5 @@ plt.show()
 #helps us versionUp the different iterations of our project. THey pull requests of the different iterations. So people can go back to previous versions 
 # Repository is in GitHub, source our version control, source control talking about a code
 #in the terminal section we are naming the source path 
+
+
--- myfirstscript.py
+++ myfirstscript.py
@@ -4,18 +4,32 @@
 x = 1
 while x < 10:
     print(x)
-    x += 1
-     #will continually run and show 1 as a loop. We need to close the loop that is showcasing. 
-    #this is is a continuous loop, we need the close the loop. 
-    #you put the breakpoint in the problem area line. Before the error shows up. 
-    #run and debug and press the continue button to test each line.
-    # chgange the value of X to get out of the loop
+    x += 1   
+#will continually run and show 1 as a loop. We need to close the loop that is showcasing. 
+#this is is a continuous loop, we need the close the loop. 
+#you put the breakpoint in the problem area line. Before the error shows up. 
+#run and debug and press the continue button to test each line.
+#change the value of X to get out of the loop
+#the while loop will run as long as the condition 'x < 10' is TRUE. 
+#the current value of x is printed in the print()
+#During the first iteration, "x" is 1, so 1 will be printed. 
+#the loop goes through 1 to 9 as X becomes ten the value becomes false. 
 
 
 # problem #2
 # use a breakpoint in line 14 to debug
-mylist = list(range(5)) # range 0 through 4 , the list function creates a list out of the number my list = [ 0, 1,2 ,3 4]
+mylist = list(range(5)) # range 0 through 4 , the list function creates a list out of the number my list = [ 0, 1, 2, 3, 4]
+#range(5) is from 0 1 2 3 4. 5 integers starting at 0. 
+#list() you converted them to a list format, why ? Because we can access individual items in lists and not ranges. 
+#we store these values a variable called my list. 
 
-for x in range(1,5):
+for x in range(1,5): #picks up at 1 and does not include 5 because it stops at 4. Range goes up unto the last digit. 
     print(f"Run No.:{mylist[x]}") # f function allows us to put everyting together and format the way we what it. 
+# the f-string allows us to put special placeholders where we will later put in values. 
+#the curly brackets is where you can put in values, we putting the list in their. 
+#the list in mylist equals 0 1 2 3 4. 
 
+
+thelist = list(range(7))
+for b in range(1,7): #ask why is it printing 4 over and over. It is because it is in the previous loop. Do i need to close the previous loop somehow. 
+    print(f"The answer is:{thelist[b]}") # need it to update to B!
